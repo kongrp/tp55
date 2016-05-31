eb2764282332051eae04531dcb44b6e27dca2026
kongrp <960139253@qq.com>
2016-05-31 12:33:02 +0800

    测试User类

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,12 +1,12 @@
 <?php
 namespace app\index\controller;
-use think\Controller;
-use think\Db;
+use app\index\model\User;
 
-class Index extends controller
+class Index
 {
     public function index()
     {	
-    	
+      $test = New User(); //实例化对象
+      dump($test);        //输出对象信息
     }
 }
