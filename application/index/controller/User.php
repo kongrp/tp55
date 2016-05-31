0fa86e14509e0ebb5934bca134d12240598223e6
kongrp <960139253@qq.com>
2016-05-31 14:10:04 +0800

    新增单个用户数据

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -7,10 +7,16 @@ class User
 	//新增用户数据
 	public function add()
 	{
+		//自增单个用户数据
 		$user = New UserModel();
 		$user->nickname = '流年';
 		$user->email = 'thinkphp@qq.com';
 		$user->birthday = strtotime('1977-03-05');
+		// //下三行与上三行作用一样
+		// $user['nickname'] = '看云';
+		// $user['email'] = 'kancloud@qq.com';
+		// $user['birthday'] = strtotime('1977-03-05');
+
 		if($user->save())
 		{
 			return '用户['.$user->nickname.':'.$user->id.']新增成功';
