78d99d929726606cf3cfe42f9f67d788f1a1a910
kongrp <960139253@qq.com>
2016-06-01 14:35:50 +0800

    read()+update()+delete()

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -48,20 +48,21 @@ class User
 
 
 	//查询用户数据
-	//现改为通过用户的email来查询模型数据
-	
-	public function read()
-{
-$user = UserModel::getByEmail('thinkphp@qq.com');
-echo $user->nickname . '<br/>';
-echo $user->email . '<br/>';
-echo date('Y/m/d', $user->birthday) . '<br/>';
-}
-
 	public function read()
 	{
-		$user = UserModel::getByEmail('thinkphp@qq.com');
+		//通过用户email来查询
+		//$user = UserModel::getByEmail('thinkphp@qq.com');
 		
+		//按照id查询，read()方法需改为read($id)
+		//$user = UserModel::get($id);
+
+		//根据nickname读取用户数据，以传入数组作为查询条件
+		//$user = UserModel::get(['nickname'=>'xingyun']); 
+
+		//根据nickname读取用户数据，以查询建构器作为查询条件
+	    $user = UserModel::where('nickname','liunian')->find();
+
+		//输出查询到的用户信息
 		echo $user->nickname.'<br/>';
 		echo $user->email.'<br/>';
 		echo date('Y/m/d',$user->birthday).'<br/>';
@@ -70,5 +71,70 @@ echo date('Y/m/d', $user->birthday) . '<br/>';
 		// echo date('Y/m/d',$user['birthday']).'<br/>';
 
 	}
+
+	public function index()
+	{
+		//主键查询
+		//$list = UserModel::all();
+
+		//可以直接传入数组条件查询
+		//$list = UserModel::all(['status'=>1]);	
+
+		//使用查询构建器
+		$list = UserModel::where('id','<',3)->select();
+
+		foreach ($list as $user) 
+		{
+			echo $user->nickname . '<br/>';
+			echo $user->email . '<br/>';
+			echo date('Y/m/d',$user->birthday) . '<br/>';
+			echo '---------------<br/>';
+		}
+	}
+
+	//update()方法
+	public function update($id)
+	{
+		// $user = UserModel::get($id);
+		// $user->nickname = "liuchen";
+		// $user->email = "liuchen@qq.com";
+		// if(false !== $user->save())
+		// {
+		// 	return '用户更新成功';
+		// }
+		// else{
+		// 	return $user->getError();
+		// }
+
+		//更为高效的方法
+		$user['id'] = (int)$id;
+		$user['nickname'] = 'liuchen1';
+		$user['email'] = 'liuchen1@qq.com';
+		$result = UserModel::update($user);
+		if(false !== $result){
+			return '用户更新成功';
+		}
+		else{
+			return $user->getError();
+		}
+	}
+
+	//删除用户信息
+	public function delete($id)
+	{
+		$user = UserModel::get($id);
+		if($user)
+		{
+			$user->delete();
+			return '删除成功';
+		}
+		else
+		{
+			return '删除不成功';
+		}
+	}
+
+
+
 	
 }
