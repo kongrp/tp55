a463b51b603cd9e99814bb9426bef04b24f61d60
kongrp <960139253@qq.com>
2016-06-01 16:40:55 +0800

    user_birthday读取器

--- application/index/model/user.php
+++ application/index/model/user.php
@@ -10,10 +10,24 @@ class User extends Model
 	// 	return date('Y-m-d',$birthday);
 	// }
 
-	//同上面的另一个写法,user_birthday读取器
-	protected function getUserBirthdayAttr($value,$data)
-	{
-		return date('Y-m-d',$data['birthday']);
-	}
+	// //同上面的另一个写法,user_birthday读取器
+	// protected function getUserBirthdayAttr($value,$data)
+	// {
+	// 	return date('Y-m-d',$data['birthday']);
+	// }
+
+	// //定义修改器
+	// protected function setBirthdayAttr($value)
+	// {
+	// 	return strtotime($value);
+	// }
+
+	//以下等同于以上方法
+	protected $dateFormat = 'Y/m/d';
+	protected $type       = [
+        'birthday' => 'datetime',
+    ];
+
+
 
 }	
