77f14b4a5dc9b571110548ad7d57d1f70f88be06
kongrp <960139253@qq.com>
2016-06-02 17:42:18 +0800

    求情与响应

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,13 +1,64 @@
 <?php
 namespace app\index\controller;
 use app\index\model\User;
+use think\Input;
 
 class Index
 {
-    public function index()
+    public function add()
     {	
-      echo "<meta charset='utf-8' />'成功退出'";
-      $test = New User(); //实例化对象
-      dump($test);        //输出对象信息
+      return view();  
     }
+
+    public function save($name = 'World')
+    {
+    	// echo 'GET原生态信息';
+    	// dump($_GET);
+    	// echo 'POST原生态信息';
+    	// dump($_POST);
+    	// echo 'GET信息';
+    	// dump(Input::get());
+    	// echo 'GET参数信息';
+    	// dump(Input::get('name'));
+    	// echo 'POST参数信息';
+    	// dump(Input::post('name'));
+    	// echo '当前请求的参数：name';
+    	// dump(Input::param('name'));
+    	// echo '当前请求的参数：username';
+    	// dump(Input::param('username'));
+    	// echo '当前请求的参数：id';
+    	// dump(Input::param('id'));
+    	// echo '当同时存在get与post数据时，我们看到param获取的是post数据.<br/>';
+    	// return 'hello'.$name.'!';
+
+    	//使用助手函数
+    	echo 'GET原生态信息';
+    	dump($_GET);
+    	echo 'POST原生态信息';
+    	dump($_POST);
+    	echo 'GET信息';
+    	dump(Input('get.'));
+    	echo 'GET参数信息';
+    	dump(Input('get.name'));
+    	echo 'POST参数信息';
+    	dump(Input('post.name'));
+    	echo '当前请求的参数：name';
+    	dump(Input('param.name'));
+    	echo '当前请求的参数：username';
+    	dump(Input('param.username'));
+    	echo '当前请求的参数：id';
+    	dump(Input('param.id'));
+    	echo '当同时存在get与post数据时，我们看到param获取的是post数据.<br/>';
+    	return 'hello'.$name.'!';
+
+
+    }
+
+
+public function hello()
+{
+return redirect('index/error',301);
+    }
+
+    
 }
